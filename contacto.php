c394710f208dc312c6906fc8dc85610967e66c92
theonemanarmy <juan_27angel@hotmail.com>
2019-07-09 15:23:38 -0500

    se está validando con javascript

--- contacto.php
+++ contacto.php
@@ -59,14 +59,14 @@
 							<div class="ajax-hidden">
 								<div class="form-group">
 									<label class="sr-only" for="c_name">Nombre</label>
-									<input type="text" id="c_name" class="form-control" name="c_name" placeholder="Nombre" >
+									<input type="text" id="c_name" onkeyup="this.className = 'campo'" class="form-control"  maxlength="100" name="c_name" placeholder="Nombre" required><span></span>
 								</div>
 								<div class="form-group">
 									<label class="sr-only" for="c_email">E-mail </label>
-									<input type="email" id="c_email" class="form-control" name="c_email" placeholder="E-mail">
+									<input type="email" id="c_email" onkeyup="this.className = 'campo'" class="form-control" name="c_email" placeholder="E-mail" required><span></span>
 								</div>
 								<div class="form-group">
-									<textarea class="form-control" id="c_message" name="c_message" rows="7" placeholder="Mensaje"></textarea>
+									<textarea class="form-control" id="c_message" onkeyup="this.className = 'campo'" name="c_message" rows="7" placeholder="Mensaje" required></textarea><span></span>
 								</div>
 								<button type="submit" class="btn btn-custom-1-inverse">
 									<i class="fa fa-bullhorn icon-before"></i> Enviar
